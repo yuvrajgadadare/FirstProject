8a32c20cc8b5464aa6458c6768c34ae330a057ce
yuvrajgadadare <42262087+yuvrajgadadare@users.noreply.github.com>
2023-11-29 14:11:42 +0530

    Initial commit

---
+++ FirstProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    internal class Program
+    {
+        public void Add()
+        {
+            int a, b, c;
+            a = 10;
+            b = 20;
+            c = a + b;
+            Console.WriteLine("Add=" + c);
+        }
+        public void Mul()
+        {
+            int a, b, c;
+            a = 50;
+            b = 10;
+            c = a * b;
+            Console.WriteLine("Mul=" + c);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Add();
+            p.Mul();
+            Console.ReadLine();
+        }
+    }
+}
